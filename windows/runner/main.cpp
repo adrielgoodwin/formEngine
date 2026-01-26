5a69a42d33d4112e57640534a207576606b1687c
Adriel Goodwin <home@Adriels-MacBook-Air.local>
2026-01-26 11:16:02 -0500

    Icon + name update

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"form_engine", origin, size)) {
+  if (!window.Create(L"Estate Intake", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
